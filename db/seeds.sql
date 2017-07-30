9662002794fbaf71e4cc22a4177b4068146713cf
Bijan Shafiei <bshafiei33@gmail.com>
2017-07-30 13:20:27 -0500

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Crusty Onion with Pimento Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Frito Pie with Queso Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken BBQ', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sticks To Your Rib with Slaw', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pinkys Too Undercooked with Chili', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joes Sloppy with Fried Pickles', true);
